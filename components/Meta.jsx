02b903eadeb24bcea7ab5221b56a2a24a2efd7f1
Kevin Grimaldi <anthgrim@icloud.com>
2022-09-26 20:57:38 -0400

    initial set up, adding meta component

---
+++ components/Meta.jsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+Meta.defaultPropr = {
+  title: "OrderQue.com",
+  keywords: "restaurant, order, web application, NextJs App, customer order",
+  description: "OrderQue, where small restaurants grow, and generate community",
+};
+
+export default Meta;
