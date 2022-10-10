c80bef9e81508db03ddf9b2c7094a4bc85c39391
Kevin Grimaldi <anthgrim@icloud.com>
2022-10-10 19:21:54 -0400

    Adding credentials to s3 client

--- pages/api/restaurant/deletePrevKey.js
+++ pages/api/restaurant/deletePrevKey.js
@@ -66,7 +66,13 @@ const handler = async (req, res) => {
     const targetKey = targetRestaurant.awsKey;
     let s3Data;
     if (targetKey !== "") {
-      const s3Client = new S3Client({ region: process.env.S3_UPLOAD_REGION });
+      const s3Client = new S3Client({
+        region: process.env.S3_UPLOAD_REGION,
+        credentials: {
+          secretAccessKey: process.env.S3_UPLOAD_SECRET,
+          accessKeyId: process.env.S3_UPLOAD_KEY,
+        },
+      });
 
       const bucketParams = {
         Bucket: process.env.S3_UPLOAD_BUCKET,
